8f74f5aee4ec6d50efc7215c4b16967139ba8719
vchuynh <73868785+vchuynh@users.noreply.github.com>
2022-04-01 23:53:30 -0400

    Added autolog command and other WIP features

--- logbot.py
+++ logbot.py
@@ -4,6 +4,8 @@ import config
 import os
 #import datetime
 import requests
+import aiohttp
+from datetime import datetime
 from enum import Enum
 from emoji import demojize
 
@@ -31,7 +33,7 @@ class Logger():
         self.nickname = config.NICKNAME
         self.token = config.TWITCH_TOKEN
         self.channel = config.CHANNEL
-        self.filename = "log.txt"
+        self.filename = "chat.txt"
         self.is_on = True
         
     async def on_pubmsg(self):
@@ -61,13 +63,22 @@ class LogBot(discord.Client):
 
         self.files = []
 
+        self.watchlist = []
+
         #self.twitch_token = config.TWITCH_TOKEN
-        
+        self.oauth_url = "https://id.twitch.tv/oauth2/token?client_id=" + config.TWITCH_CLIENT_ID + "&client_secret=" \
+                         + config.TWITCH_CLIENT_SECRET + "&grant_type=client_credentials"
+        self.access_token = None
         #channel_dict = {}
 
         self.is_logging = False
+        self.is_auto_logging = False
         self.chat_logger = Logger()
         super().__init__(*args, **kwargs)
+        
+    def initialize(self):
+        self.set_access_token()
+        self.update_filelist()
 
     def update_filelist(self):
         for file in os.listdir():
@@ -75,24 +86,41 @@ class LogBot(discord.Client):
                 self.files.append(file)
                 #TODO: intialize dictionaries for future filtering and access
                 #file.split("_")
-    def check_status(self, user):
-        info = None
+            elif file == "watchlist.txt":
+                with open("watchlist.txt") as file_obj:
+                    for line in file_obj:
+                        self.watchlist.append(line)
+
+    def set_access_token(self):
+        resp = requests.post(self.oauth_url, timeout=15)
+        resp.raise_for_status()
+        token = resp.json()
+        self.access_token = token["access_token"]
+
+    async def get_status(self, user):
         status = TwitchStatus.ERROR
+        stream_data = None
         try:
-            headers = {"Client-ID": config.CLIENT_ID, "Authorization": "Bearer " + config.TWITCH_TOKEN}
-            r = requests.get("https://api.twitch.tv/helix/streams?user_login=" + user, headers = headers, timeout = 15)
-            r.raise_for_status()
-            info = r.json()
-            if info is None or not info["data"]:
+            headers = {"Client-ID": config.TWITCH_CLIENT_ID, "Authorization": "Bearer " + self.access_token}
+            async with aiohttp.ClientSession(headers = headers) as session:
+                async with session.get("https://api.twitch.tv/helix/streams?user_login=" + user) as r:
+                    r.raise_for_status()
+                    stream_data = await r.json()
+            if stream_data is None or not stream_data["data"]:
                 status = TwitchStatus.OFFLINE
+                print("offline") #test
             else:
                 status = TwitchStatus.ONLINE
-        except requests.exceptions.RequestException as err:
-            if err.response.status_code == 401:
+                print("online") #test
+        except aiohttp.ClientResponseError as err:
+            if err.status == 401:
                 status = TwitchStatus.UNAUTHORIZED
-            if err.response.status_code == 404:
+                self.set_access_token()
+                print("unauthorized") #test
+            elif err.status == 404:
                 status = TwitchStatus.NOT_FOUND
-        return info, status
+                print("not found") #test
+        return status, stream_data
     
     async def on_ready(self):
         print('We have logged in as {0.user}'.format(self))
@@ -111,7 +139,7 @@ class LogBot(discord.Client):
             elif not config.CLIENT_ID or not config.CLIENT_SECRET:
                 await message.channel.send("Client id and secret not registered")
             else:
-                info, status = self.check_status(words[1])
+                status, stream_data = self.get_status(words[1])
                 if status == 0:
                     await message.channel.send("{} is live".format(words[1]))
                 elif status == 1:
@@ -147,6 +175,7 @@ class LogBot(discord.Client):
             self.update_filelist()
             self.chat_logger.is_on = False    
             self.is_logging = False
+            self.is_auto_logging = False
             await message.channel.send("Logger for {} is stopped".format(self.chat_logger.channel))
         elif message.content.startswith("$logs"):
             await message.channel.send(self.files)
@@ -161,9 +190,61 @@ class LogBot(discord.Client):
             else:
                 await message.channel.send("Uploading Log {}".format(words[1]))
                 await message.channel.send(file = discord.File(words[1]))
+        elif message.content.startswith("$add"):
+            words = message.content.split()
+            if len(words) != 2:
+                await message.channel.send("Invalid usage of upload command")
+            elif words[1] in self.watchlist:
+                await message.channel.send("Channel already in watchlist")
+            else:
+                self.watchlist.append(words[1])
+                await message.channel.send("{} added to watchlist".format(words[1]))
+        elif message.content.startswith("$remove"):
+            words = message.content.split()
+            if len(words) != 2:
+                await message.channel.send("Invalid usage of upload command")
+            elif words[1] not in self.watchlist:
+                await message.channel.send("Channel not in watchlist")
+            else:
+                self.watchlist.remove(words[1])
+                await message.channel.send("{} removed from watchlist".format(words[1]))
+        elif message.content.startswith("$autolog"):
+            words = message.content.split()
+            if self.is_logging:
+                await message.channel.send("Already logging")
+            elif len(words) > 2:
+                await message.channel.send("Invalid usage of start command")
+            elif self.is_logging:
+                await message.channel.send("Already logging {}'s channel".format(self.chat_logger.channel))
+            else:
+                status = TwitchStatus.OFFLINE
+                self.is_logging = True
+                self.is_auto_logging = True
+                while status != TwitchStatus.ONLINE:
+                    status, stream_data = await self.get_status(words[1])
+                    if status == TwitchStatus.ONLINE:
+                        self.chat_logger.channel = "#" + words[1]
+                        self.num_logging += 1
+                        self.chat_logger.is_on = True
+                        self.is_logging = True
+                        self.chat_logger.filename = words[1] + " " + datetime.now().strftime("%Y-%m-%d %Hh%Mm%Ss") + ".txt"
+                        await message.channel.send("Auto Logger for {} is running".format(self.chat_logger.channel))
+                        embed = discord.Embed(title = "{} is live".format(words[1]), thumbnail = stream_data["data"][0]["thumbnail_url"].format(width = 1280, height = 720), )
+                        await message.channel.send(embed = embed)
+                        loop = asyncio.get_event_loop()
+                        loop.create_task(self.chat_logger.on_pubmsg())
+                        break
+                    if not self.is_auto_logging:
+                        print("HEY ITS DONE")
+                        break
+                    await asyncio.sleep(15)
+
+
+                
+
 def main():
     client = LogBot()
-    client.update_filelist()
+    client.initialize()
     client.run(config.DISCORD_TOKEN)
 
 if __name__ == "__main__":
